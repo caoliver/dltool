2582e36367d6110024b573450106a4abc129ef97
Christopher A. Oliver <caowasteland@gmail.com>
2021-07-23 12:25:23 -0400

    Deal with directories with missing trailing slash

--- dltool.lua
+++ dltool.lua
@@ -29,12 +29,12 @@ function resolve (directories, prefix, extralibs, cluster,
 
    local function cleanuppath(path)
       local stack = {}
-      local first,rest=path:match('([^/]*/?)(.*)')
+      local first,rest=(path..'/'):match('([^/]*/?)(.*)')
       local anchored = first == '/'
       local backcount = 0
       if first == '..' or first == '../' then backcount = 1 end
       table.insert(stack, first)
-      for part in string.gmatch(rest, '([^/]*/?)') do
+      for part in string.gmatch(rest..'/', '([^/]*/?)') do
 	 if #part > 0 and part ~= '/' and part ~= './' then
 	    if part ~= '..' and part ~= '../' then
 	       table.insert(stack,part)
@@ -46,10 +46,16 @@ function resolve (directories, prefix, extralibs, cluster,
 	    end
 	 end
       end
+      --[[
+      if #stack > 1 and stack[#stack] == '.' then
+	 table.remove(stack, #stack)
+      end
       if #stack > 1 or #stack[1] > 1 then
 	 local first=stack[#stack]:match('^(.*)/')
 	 if first then stack[#stack] = first end
       end
+      --]]
+      
       return table.concat(stack, '')
    end
 
